4d4aa1d9efceebbf2f36b16e8432dd2804a22279
jieron <64154603+jieron@users.noreply.github.com>
2020-04-23 08:10:23 +0300

    Update ji.js

--- ji.js
+++ ji.js
@@ -1,15 +1,20 @@
 $( document ).ready(function() {
+  document.getElementById("snd").style.display='none';
   console.log( "ready!" );
-
-  var x =document.getElementById('snd');
-  x.id="newsnd";
-  console.log("=>");
+  const btn = document.createElement('button');
+  btn.id='sendBtn';
+  btn.innerText='=>';
+  btn.className ='img-btn';
+  btn.onclick=function show(){
+    sendMsg();
+    console.log(":::::");
+  }
+  const x =document.getElementsByClassName('foot')[0];
+  x.appendChild(btn);
+  console.log('=>');
 
 });
-
-$("#newsnd").click(function() {
-    
-
+function sendMsg() {
   if( !$(".ta").val() ) { return false; }
   tav = $(".ta").val();
   console.log(tav);
@@ -19,22 +24,29 @@ $("#newsnd").click(function() {
   else
   {
    setTimeout(function() {
-     console.log("http://araby.co/ajax.php?c=s&rm="+$("#rm").val()+"&fr="+$("#myid").val()+"&to=fcwxn"+$(".ta").attr("id").replace(/.+q9j1w6/,"")+"&txt="+tav);
-    $.get("/ajax.php?c=s&rm="+$("#rm").val()+"&fr="+$("#myid").val()+"&to=fcwxn"+$(".ta").attr("id").replace(/.+q9j1w6/,"")+"&txt="+tav, function(data) {
+
+    const url1 = "http://araby.co/ajax.php?c=s&rm="+$("#rm").val()+"&fr="+$("#myid").val()+"&to=fcwxn"+$(".ta").attr("id").replace(/.+q9j1w6/,"")+"&txt="+tav ;
+    console.log("AXIOS");
+    axios(
+      {
+          url:url1 ,
+          method:'GET',
+          headers:{'Content-Type':'text/html', 'Content-Encoding': 'identity',} 
+      }
+  ).then(function(data){
     if (data === 'done') {
-     $('<div class="me-div"><p class="me">'+tav+'</p></div>').appendTo(".conv#"+$(".ta").attr("id"));
-     $(".conv#"+$(".ta").attr("id")).scrollTop($(".conv#"+$(".ta").attr("id")).height());
-    }
-    else if (data === 'out')
-    {
-     $(".users #"+$(".ta").attr("id")).remove();
-     alert("المستخدم غادر الغرفة");
-    }
-    else
-     alert("لم يتم إرسال الرسالة");
-    });
-   }, 1000);
+      $('<div class="me-div"><p class="me">'+tav+'</p></div>').appendTo(".conv#"+$(".ta").attr("id"));
+      $(".conv#"+$(".ta").attr("id")).scrollTop($(".conv#"+$(".ta").attr("id")).height());
+     }
+     else if (data === 'out')
+     {
+      $(".users #"+$(".ta").attr("id")).remove();
+      alert("المستخدم غادر الغرفة");
+     }
+  }).catch(function(err){
+    alert("لم يتم إرسال الرسالة");
+  });}, 1000);
   }
- });
+ };
  
  $(".ta").keypress(function(e) { if (e.which === 13) { e.preventDefault(); $("#snd").click(); } });
